e503f4e8b1f1a61d628b8c7f840837533e0bd599
sifeddineEddr <sifeddine.edr@gmail.com>
2024-07-10 19:04:48 +0100

    seting up the person abstract class && derived user(staff) and client classes schema

--- core/custom_user/models.py
+++ core/custom_user/models.py
@@ -1,5 +1,36 @@
 from django_use_email_as_username.models import BaseUser, BaseUserManager
+from django.db import models 
 
+# abstract class (not created in the database and used as a base classe)
+class Person(models.Model):
+    cin = models.CharField(max_length=20 ,unique=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    
+    class Meta:
+        abstract = True
 
-class User(BaseUser):
+# the user class represents the staff interacting with the system
+class User(BaseUser, Person):
+    ROLES = [
+        ('admin', 'admin'),
+        ('manager', 'manager'),
+        ('agent', 'agent'),
+    ]
+
+    STATUS = [
+        ('actif', 'actif'),
+        ('démissionné', 'démissionné'),
+        ('décédé', 'décédé'),
+        ('retraite', 'retraite'),
+    ]
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLES, default='agent')
+    status = models.CharField(max_length=20, choices=STATUS, default='actif')
+
     objects = BaseUserManager()
+
+
+class Client(Person):
+    phone_number = models.CharField(max_length=12)
