adb0ea17f55e677b6cbe4cc3b76cfff34b1c40ff
sifeddineEddr <sifeddine.edr@gmail.com>
2024-07-12 23:09:11 +0100

    updating permissions by adding 'actif' status check

--- core/custom_user/permissions.py
+++ core/custom_user/permissions.py
@@ -1,5 +1,9 @@
 from rest_framework.permissions import BasePermission, IsAuthenticated
 
+# helper function to check if the user has the required role and status
+def role_and_status_check(user, role):
+    return user.role == role and user.status == 'actif'
+
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
 
@@ -7,18 +11,18 @@ class IsAdmin(BasePermission):
         if not IsAuthenticated().has_permission(request, view):
             return False
             
-        return request.user.role == 'admin'
+        return role_and_status_check(request.user, 'admin')
     
 class IsManager(BasePermission):
     def has_permission(self, request, view):
         if not IsAuthenticated().has_permission(request, view):
             return False
         
-        return request.user.role == 'manager'
+        return role_and_status_check(request.user, 'manager')
 
 class IsAgent(BasePermission):
     def has_permission(self, request, view):
         if not IsAuthenticated().has_permission(request, view):
             return False
         
-        return request.user.role == 'agent'
+        return role_and_status_check(request.user, 'agent')
