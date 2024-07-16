a1adef97478953f6062b37c64c1c4f28054d26e9
sifeddineEddr <sifeddine.edr@gmail.com>
2024-07-16 12:11:06 +0100

    product views.py

--- core/product/views.py
+++ core/product/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Product
+from .serializers import ProductSerializer
+from custom_user.permissions import IsAdmin
 
 # Create your views here.
+
+class ProductsInfo(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        products = Product.objects.all()
+
+        return Response(ProductSerializer(products, many=True).data, status=200)
+    
+class ProductUpdate(APIView):
+    permission_classes = [IsAdmin]
+
+    def patch(self, request, id):
+        # prevent sequence from being updated
+        if "sequence" in request.data:
+            del request.data["sequence"]
+
+        product = Product.objects.get(id=id)
+        serializer = ProductSerializer(product, data=request.data, partial=True)   
